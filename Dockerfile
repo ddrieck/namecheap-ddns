6e9d8cfb026b728982a21e0624739111f6466e49
Dean Rieck <ddrieck@gmail.com>
2021-03-06 18:19:49 -0800

    Added dockerfile and logging.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM frolvlad/alpine-python3
+
+WORKDIR /opt/namecheap-ddns
+COPY . .
+
+RUN pip install -r requirements.txt
+
+COPY ddns-cron /etc/crontabs/ddns-cron
+RUN chmod 0644 /etc/crontabs/ddns-cron
+
+CMD ["crond"]
--- ddns_update.py
+++ ddns_update.py
@@ -1,6 +1,11 @@
 import socket
 import requests
 import configparser
+import logging
+from datetime import datetime
+
+logging.basicConfig(filename="log.txt", level=logging.INFO)
+logfile = "log.txt"
 
 def remote_ip(domain):
     ip = socket.gethostbyname(domain)
@@ -23,10 +28,17 @@ def main():
 
     remote = remote_ip(domain)
     local = local_ip()
-    
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    logging.info("\n==== BEGINNING CHECK AT: %s ====" % (dt_string))
     if remote != local:
         for section in config.sections():
             set_ip(domain, config[section]['Host'], ddns_password, local)
+            logging.info("\nLocal IP: %s Remote IP: %s \nChanged DNS address for %s." % (local, remote, config[section]['Host']))
+    else:
+        logging.info("\nLocal IP: %s Remote IP: %s IPs match. No changes." % (local, remote))
 
 if __name__ == '__main__':
     main()
